27f12610f457d9173dd5b901aa996e75ad2ac37d
Ivan Shevchenko <ivan@giantsource.com>
2016-04-08 17:56:09 +0300

    IP conf

--- lineageAPi/lineageAPi.py
+++ lineageAPi/lineageAPi.py
@@ -17,6 +17,7 @@ app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.run(host='0.0.0.0')
 
 
 # extensions
