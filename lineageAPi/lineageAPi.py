0385f508d007af59140d20934d1ab40fc95e0c6d
Ivan Shevchenko <ivan.shevchenko@flavosil.com.ua>
2016-04-10 15:44:35 +0300

    fixes

--- lineageAPi/lineageAPi.py
+++ lineageAPi/lineageAPi.py
@@ -56,12 +56,16 @@ class User(db.Model):
 def verify_password(username_or_token, password):
     # first try to authenticate by token
     user = User.verify_auth_token(username_or_token)
+    print "ver section"
     if not user:
         # try to authenticate with username/password
         user = User.query.filter_by(username=username_or_token).first()
+        print "user " + str(user.id)
         if not user or not user.verify_password(password):
+            print "wrong password"
             return False
     g.user = user
+    print username_or_token, password
     return True
 
 
@@ -136,7 +140,7 @@ def hello_world():
 if __name__ == '__main__':
     if not os.path.exists('db.sqlite'):
         db.create_all()
-    app.run()
+    app.run(debug=True)
 
 
 
